791bb4a5eacb5d7c07194d02304d2b0cc1bd1d11
yoneyama ryo <uryonym@gmail.com>
2024-07-22 22:56:25 +0900

    TanStack Queryのインストール、セットアップ

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,12 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
+const queryClient = new QueryClient()
+
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <QueryClientProvider client={queryClient}>
       <div>
         <Link to='/'>ホーム</Link>
         <Link to='/task'>タスク</Link>
@@ -11,6 +14,6 @@ export const Route = createRootRoute({
       <hr />
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </QueryClientProvider>
   ),
 })

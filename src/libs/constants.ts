9775ab2d17d206dfe4f9d9d4dee8f2493694b67d
yoneyama ryo <uryonym@gmail.com>
2024-08-31 06:48:34 +0900

    API接続先を本番に修正

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -1,2 +1,2 @@
-// export const BASE_API_URL = 'https://portal-api.uryonym.com/api/v1'
-export const BASE_API_URL = 'http://localhost:3000/api/v1'
+export const BASE_API_URL = 'https://portal-api.uryonym.com/api/v1'
+// export const BASE_API_URL = 'http://localhost:3000/api/v1'

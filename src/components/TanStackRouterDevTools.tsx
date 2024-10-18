a8314cc9c656a05b07d3553248df4d44f35c8bb7
yoneyama ryo <uryonym@gmail.com>
2024-10-18 19:49:26 +0900

    本番環境でTanStack Routerの開発者ツールが表示されないように修正

--- src/components/TanStackRouterDevTools.tsx
+++ src/components/TanStackRouterDevTools.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
 export const TanStackRouterDevtools =
-  // process.env.NODE_ENV === 'production'
-  //   ? () => null
-  // :
-    React.lazy(() =>
+  process.env.NODE_ENV === 'production'
+    ? () => null
+    : React.lazy(() =>
         import('@tanstack/router-devtools').then((res) => ({
           default: res.TanStackRouterDevtools,
         })),

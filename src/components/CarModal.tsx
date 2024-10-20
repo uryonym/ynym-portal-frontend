906faa656d23afd4d9e0af5254d1f62feff94213
yoneyama ryo <uryonym@gmail.com>
2024-10-20 16:14:32 +0900

    車両編集モーダルのデザインを修正

--- src/components/CarModal.tsx
+++ src/components/CarModal.tsx
@@ -1,5 +1,6 @@
 import { FormEventHandler, useState } from 'react'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { FaTimes } from 'react-icons/fa'
 import { Car } from '../models/Car'
 import { createCar, deleteCar, updateCar } from '../api/carApi'
 import ConfirmationModal from './ConfirmationModal'
@@ -66,83 +67,81 @@ export default function CarModal({ show, onClose, car }: CarModalProps) {
 
   if (show) {
     return (
-      <>
-        <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'>
-          <div className='bg-white p-4 rounded max-w-sm w-full'>
-            <div className='text-right'>
-              <button className='underline' type='button' onClick={onClose}>
-                閉じる
-              </button>
-            </div>
-            <h2 className='text-2xl font-bold mb-2'>車両の{car ? '編集' : '新規作成'}</h2>
-            <form onSubmit={handleClickRegister}>
-              <div className='mb-2'>
-                <div>
-                  <label>車名</label>
-                </div>
-                <input type='text' name='name' defaultValue={car?.name} />
-              </div>
-              <div className='mb-2'>
-                <div>
-                  <label>Seq</label>
-                </div>
-                <input type='text' name='seq' defaultValue={car?.seq} />
-              </div>
-              <div className='mb-2'>
-                <div>
-                  <label>メーカー</label>
-                </div>
-                <input type='text' name='maker' defaultValue={car?.maker} />
-              </div>
-              <div className='mb-2'>
-                <div>
-                  <label>型式</label>
-                </div>
-                <input type='text' name='model' defaultValue={car?.model} />
-              </div>
-              <div className='mb-2'>
-                <div>
-                  <label>年式</label>
-                </div>
-                <input type='text' name='modelYear' defaultValue={car?.modelYear} />
-              </div>
-              <div className='mb-2'>
-                <div>
-                  <label>ナンバープレート</label>
-                </div>
-                <input type='text' name='licensePlate' defaultValue={car?.licensePlate} />
-              </div>
-              <div className='mb-2'>
-                <div>
-                  <label>燃料タンク</label>
-                </div>
-                <input type='text' name='tankCapacity' defaultValue={car?.tankCapacity} />
-              </div>
-              <div className='flex justify-between'>
-                <button className='px-4 py-2 bg-blue-500 text-white rounded underline' type='submit'>
-                  登録
-                </button>
-                {car && (
-                  <>
-                    <button
-                      className='px-4 py-2 bg-red-500 text-white rounded underline'
-                      type='button'
-                      onClick={() => setConfirmShow(true)}
-                    >
-                      削除
-                    </button>
-                    <ConfirmationModal
-                      show={confirmShow}
-                      onExec={() => handleClickDelete(car?.id!)}
-                      onClose={() => setConfirmShow(false)}
-                    />
-                  </>
-                )}
-              </div>
-            </form>
+      <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'>
+        <div className='bg-white p-4 rounded max-w-sm w-full'>
+          <div className='text-right'>
+            <button className='underline' type='button' onClick={onClose}>
+              <FaTimes />
+            </button>
           </div>
+          <h2 className='text-2xl font-bold mb-2'>車両の{car ? '編集' : '新規作成'}</h2>
+          <form onSubmit={handleClickRegister}>
+            <div className='mb-2'>
+              <div>
+                <label>車名</label>
+              </div>
+              <input className='w-full' type='text' name='name' defaultValue={car?.name} />
+            </div>
+            <div className='mb-2'>
+              <div>
+                <label>Seq</label>
+              </div>
+              <input className='w-full' type='text' name='seq' defaultValue={car?.seq} />
+            </div>
+            <div className='mb-2'>
+              <div>
+                <label>メーカー</label>
+              </div>
+              <input className='w-full' type='text' name='maker' defaultValue={car?.maker} />
+            </div>
+            <div className='mb-2'>
+              <div>
+                <label>型式</label>
+              </div>
+              <input className='w-full' type='text' name='model' defaultValue={car?.model} />
+            </div>
+            <div className='mb-2'>
+              <div>
+                <label>年式</label>
+              </div>
+              <input className='w-full' type='text' name='modelYear' defaultValue={car?.modelYear} />
+            </div>
+            <div className='mb-2'>
+              <div>
+                <label>ナンバープレート</label>
+              </div>
+              <input className='w-full' type='text' name='licensePlate' defaultValue={car?.licensePlate} />
+            </div>
+            <div className='mb-2'>
+              <div>
+                <label>燃料タンク</label>
+              </div>
+              <input className='w-full' type='text' name='tankCapacity' defaultValue={car?.tankCapacity} />
+            </div>
+            <div className='flex justify-between'>
+              <button className='px-4 py-2 bg-blue-500 text-white rounded underline' type='submit'>
+                登録
+              </button>
+              {car && (
+                <>
+                  <button
+                    className='px-4 py-2 bg-red-500 text-white rounded underline'
+                    type='button'
+                    onClick={() => setConfirmShow(true)}
+                  >
+                    削除
+                  </button>
+                  <ConfirmationModal
+                    show={confirmShow}
+                    onExec={() => handleClickDelete(car?.id!)}
+                    onClose={() => setConfirmShow(false)}
+                  />
+                </>
+              )}
+            </div>
+          </form>
         </div>
-      </>
+      </div>
     )
   } else {
     return

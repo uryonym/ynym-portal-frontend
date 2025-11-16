48dd281e59caaecb518f90f4cba3d3f1eb312c06
yoneyama ryo <uryonym@gmail.com>
2025-11-16 16:46:37 +0900

    main要素のスタイルを更新し、中央揃えのレイアウトを適用

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <AppSidebar />
       <SidebarInset>
         <Header />
-        <main className="flex-1 p-6">
+        <main className="flex-1 flex flex-col items-center justify-center">
           <p>ynym portal site.</p>
         </main>
       </SidebarInset>

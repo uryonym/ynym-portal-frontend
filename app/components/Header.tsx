b139d7ef5354d587f5993a5d661e1334e58ed2a5
yoneyama ryo <uryonym@gmail.com>
2025-11-16 16:26:44 +0900

    Headerコンポーネントの追加とレイアウトの調整

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="h-16 bg-white border-b border-gray-200 flex items-center">
+      <div className="px-6">
+        <h1 className="text-2xl font-bold text-gray-900">Ynym Portal</h1>
+      </div>
+    </header>
+  )
+}

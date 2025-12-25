3bdca67281e4a3abd69f73b44e97a06db8bc0d3a
yoneyama ryo <uryonym@gmail.com>
2025-12-25 22:47:38 +0900

    画像のリモートパターンを追加し、Googleユーザーコンテンツからの画像を許可

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig
